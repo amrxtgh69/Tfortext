1cfac931145fc20e87ea2aa0220f94bb39c5cdee
amrxtgh <ghimireamrit619@gmail.com>
2025-09-23 23:13:59 +0545

    update

--- src/editor/EditorFrame.java
+++ src/editor/EditorFrame.java
@@ -4,7 +4,13 @@ import javax.swing.*;
 import javax.awt.*;
 
 public class EditorFrame extends JFrame {
-  private JTextArea textArea;
-  
+  private JTextArea textArea; //this is private because it belongs to the object and can be used anywhere un the class 
+
+  //constructor
+  public EditorFrame() {
+    super("Tfortext");
+
+    textArea = new JTextArea();
+  }
 }
   
